b447b41bc9ca3c46e9180863987f0ce6a5d4ec36
hcldemo-app <hcldemo-app>
2019-09-13 13:06:23 +0000

    pushed updated 1

--- GICMicro/Controllers/HomeController.cs
+++ GICMicro/Controllers/HomeController.cs
@@ -5,13 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GICMicro.Models;
+using Microsoft.Extensions.Logging;
 
 namespace GICMicro.Controllers
 {
     public class HomeController : Controller
     {
+        ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            try
+            {
+
+                _logger.LogInformation($"List of Dashboard Data retreived on : {DateTime.UtcNow}");
+
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Web part of Application has critical error : {DateTime.UtcNow}", ex);
+                // _logger.LogCritical("Web part of Application has critical error  : {DateTime.UtcNow}, ex);
+                //  _logger.LogError(ex, "ur code iz buggy.");
+            }
+            //  AddLo
+
             return View();
         }
 
